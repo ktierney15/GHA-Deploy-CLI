8b4c3379ce0b54fbd040afdd47889c0319315a53
ktierney15 <kevintierney99@gmail.com>
2025-04-08 15:47:23 -0400

    added small unit test

--- tests/test_api.py
+++
@@ -1,3 +0,0 @@
-@patch('gha_deploy.github_api.requests.post')
-def test_trigger_workflow(mock_post):
-    pass
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,13 @@
+from click.testing import CliRunner
+from unittest.mock import patch
 
+from gha_deploy.cli import deploy
 
-def test_deploy_command():
-    result = runner.invoke(cli.deploy, ['--repo', 'GHA-Deploy-CLI', '--workflow', 'Unit-Test.yml', '--ref', 'main'])
+def test_deploy_cli():
+    runner = CliRunner()
+    result = runner.invoke(deploy, ["GHA-Deploy-CLI", "Unit-Test.yml", "main"])
+
     assert result.exit_code == 0
+    assert "GitHub Action triggered successfully!" in result.output
+    assert "Waiting for Job to finish..." in result.output
+    assert "Workflow completed successfully! ✅" in result.output
